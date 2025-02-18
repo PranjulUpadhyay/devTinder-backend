55186e74c101680f2b8298d24577f5c264050ea7
Pranjul Upadhyay <pranjulupadhyay775@gmail.com>
2025-02-18 18:14:12 +0530

    Learn about Multiple Request Handlers

--- src/app.js
+++ src/app.js
@@ -8,51 +8,94 @@ app.listen(3000, ()=>{
     console.log("Server Started...");
 });
 
-// GET request handle
-app.get("/user", (req, res) => {
-    // console.log(req.query);
-    res.send({firstname: "Pranjul", lastname: "Upadhyay"});
-});
 
-app.get("/user/:userID/:name/:password", (req, res) => {
-    console.log(req.param);
-    res.send({firstname: "Pranjul", lastname: "Upadhyay"});
-});
+// Multiple Request Handlers
+app.use("/user",
+    (req, res, next) => {
+        next();
+        console.log("Handled Handler 1");
+        res.send("Request 1");
+    },
 
-// POST reuest handle
-app.post("/user", (req, res) => {
-    res.send("Data Saved Successfully");
-});
+    (req, res, next) => {
+        console.log("Handles Handler 2");
+        // res.send("Request 2");
+        next();
+    },
 
-// DELETE request handle
-app.delete("/user", (req, res) => {
-    res.send("User Deleted");
-});
+    (req, res, next) => {
+        console.log("Handles Handler 3");
+        // res.send("Request 3");
+        next();
+    },
 
-// PATCH request handle
-app.patch("/user", (req, res) => {
-    res.send("Patched!!!");
-});
+    (req, res, next) => {
+        console.log("Handles Handler 4");
+        // res.send("Request 4");
+        next();
+    },
+
+    (req, res, next) => {
+        console.log("Handles Handler 5");
+        res.send("Request 5");
+        // next();
+    }
+);
 
 
 
-// Adavanced Routing Concepts
-app.get("/a(bd)?c", (req, res) => {
-    res.send("abc ? wala");
-});
 
-app.get("/AB+C", (req, res) => {
-    res.send("ABC + wala");
-});
 
-app.get("/EF*G", (req, res) => {
-    res.send("EFG * wala");
-});
 
-app.get(/p/, (req, res) => {
-    res.send("P regex wala");
-});
 
-app.get(/.*fly$/, (req, res) => {
-    res.send("Fly wala");
-});
+
+
+
+// // GET request handle
+// app.get("/user", (req, res) => {
+//     // console.log(req.query);
+//     res.send({firstname: "Pranjul", lastname: "Upadhyay"});
+// });
+
+// app.get("/user/:userID/:name/:password", (req, res) => {
+//     console.log(req.param);
+//     res.send({firstname: "Pranjul", lastname: "Upadhyay"});
+// });
+
+// // POST reuest handle
+// app.post("/user", (req, res) => {
+//     res.send("Data Saved Successfully");
+// });
+
+// // DELETE request handle
+// app.delete("/user", (req, res) => {
+//     res.send("User Deleted");
+// });
+
+// // PATCH request handle
+// app.patch("/user", (req, res) => {
+//     res.send("Patched!!!");
+// });
+
+
+
+// // Adavanced Routing Concepts
+// app.get("/a(bd)?c", (req, res) => {
+//     res.send("abc ? wala");
+// });
+
+// app.get("/AB+C", (req, res) => {
+//     res.send("ABC + wala");
+// });
+
+// app.get("/EF*G", (req, res) => {
+//     res.send("EFG * wala");
+// });
+
+// app.get(/p/, (req, res) => {
+//     res.send("P regex wala");
+// });
+
+// app.get(/.*fly$/, (req, res) => {
+//     res.send("Fly wala");
+// });
