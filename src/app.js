9c6fa7b47d61d820c9dcbb8a158b44917c38afd4
Pranjul Upadhyay <pranjulupadhyay775@gmail.com>
2025-02-18 16:59:14 +0530

    Learn Advanced Routing Concepts

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ app.listen(3000, ()=>{
 
 // GET request handle
 app.get("/user", (req, res) => {
+    // console.log(req.query);
+    res.send({firstname: "Pranjul", lastname: "Upadhyay"});
+});
+
+app.get("/user/:userID/:name/:password", (req, res) => {
+    console.log(req.param);
     res.send({firstname: "Pranjul", lastname: "Upadhyay"});
 });
 
@@ -28,3 +34,25 @@ app.patch("/user", (req, res) => {
     res.send("Patched!!!");
 });
 
+
+
+// Adavanced Routing Concepts
+app.get("/a(bd)?c", (req, res) => {
+    res.send("abc ? wala");
+});
+
+app.get("/AB+C", (req, res) => {
+    res.send("ABC + wala");
+});
+
+app.get("/EF*G", (req, res) => {
+    res.send("EFG * wala");
+});
+
+app.get(/p/, (req, res) => {
+    res.send("P regex wala");
+});
+
+app.get(/.*fly$/, (req, res) => {
+    res.send("Fly wala");
+});
